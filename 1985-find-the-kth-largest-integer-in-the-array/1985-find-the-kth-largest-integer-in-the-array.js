2ea64c1271601d7c1f1bc4f8f8f65bfef8969c42
rewinaz <79724392+rewinaz@users.noreply.github.com>
2022-09-24 23:21:00 +0400

    Time: 10492 ms (5.00%), Space: 53.4 MB (97.50%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} nums
+ * @param {number} k
+ * @return {string}
+ */
+var kthLargestNumber = function(nums, k) {
+      let n = nums.length;
+
+  let key, j;
+  for(let i = 1; i<n;i++) {
+      key=BigInt(nums[i]);
+      j=i-1;
+      
+      while(j>=0 && BigInt(nums[j]) > key) {
+        nums[j+1] = nums[j];
+          j--;
+      }
+      nums[j+1] = key;
+  }
+
+  
+  return `${nums[n-k]}`;
+};
