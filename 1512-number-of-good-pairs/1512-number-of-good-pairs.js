5535a38f7d2243aa80c9088c501b10584eab5c9e
rewinaz <79724392+rewinaz@users.noreply.github.com>
2022-09-26 12:40:51 +0400

    Time: 63 ms (93.56%), Space: 41.7 MB (87.30%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let count=0;
+  for(let i=0;i<nums.length;i++) {
+      for(let j=0;j<nums.length;j++) {
+          if(i!=j && i<j) {
+              if(nums[i]===nums[j])
+                  count++;
+          }
+      }
+  }
+  
+  return count;
+};
