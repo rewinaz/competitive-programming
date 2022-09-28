3aad2e15dabc34fcb685ad495afa0df9e7f3d467
rewinaz <79724392+rewinaz@users.noreply.github.com>
2022-09-29 00:40:20 +0400

    Time: 349 ms (5.90%), Space: 48.5 MB (5.13%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let l=0, r=nums.length-1;
+    let temp;
+    
+    while(l<r) {
+        if(nums[l] === 0&&nums[r]===0) {
+            r--;
+        }else if(nums[l] === 0){
+            swap(nums,l,r)
+            r--;
+        }else {
+            l++;
+        }
+        
+    }
+    console.log(nums)
+};
+
+
+function swap(nums, l,r) {
+    let i=l;
+            while(i<r) {
+                  temp=nums[i]
+                    nums[i]=nums[i+1]
+                    nums[i+1]=temp
+                    i++
+                  }
+}
