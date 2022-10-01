a30926a39dbc410b8ebfd8922d6cdbb0d6e30096
rewinaz <79724392+rewinaz@users.noreply.github.com>
2022-10-01 18:02:54 +0400

    Time: 867 ms (97.77%), Space: 20.6 MB (6.10%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,15 +1,23 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        start = 0
-        end = 0
-        max_length = 0
-        fruit = {}
-        while end < len(fruits):
-            fruit[fruits[end]] = end
-            if len(fruit) >= 3:
-                least = min(fruit.values())
-                del fruit[fruits[least]]
-                start = least + 1
-            max_length = max(max_length, end - start + 1)
-            end += 1
-        return max_length
+        last_fruit = -1
+        second_last_fruit = -1
+        last_fruit_count = 0
+        current_max=0
+        max_fruit_count = 0
+        for i in range(0, len(fruits)):
+            if fruits[i] == last_fruit or fruits[i] == second_last_fruit:
+                current_max+=1
+            else:
+                current_max = last_fruit_count +1
+            
+            if(fruits[i] == last_fruit):
+                last_fruit_count+=1
+            else:
+                last_fruit_count = 1
+            
+            if fruits[i] != last_fruit:
+                second_last_fruit = last_fruit
+                last_fruit = fruits[i]
+            max_fruit_count = max(max_fruit_count, current_max)
+        return max_fruit_count
